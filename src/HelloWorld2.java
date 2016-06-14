e5cc446d7e3c1c5dc31c73b6b7c48b5b263684ab
Abhay Dang <dangabhay@gmail.com>
2016-06-14 15:38:10 +0530

    Change in helloworld2

--- src/HelloWorld2.java
+++ src/HelloWorld2.java
@@ -2,7 +2,7 @@
 public final class HelloWorld2 {
 
 	public static void main(String[] args) {
-		System.out.println("This is also hello world");
+		System.out.println("This is also hello world, yippee");
 
 	}
 
