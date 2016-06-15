ef4e0c04ffcc6bfbf3fbc5fcf4df70cbbf79f71f
Abhay Dang <dangabhay@gmail.com>
2016-06-15 11:40:34 +0530

    Adding lombok jar

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,3 +1,5 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 public final class HelloWorld {
 
@@ -6,7 +8,17 @@ public final class HelloWorld {
 			System.out.println("Hello " + args[0]);
 		else
 			System.out.println("No argument supplied");
+		
+		MyClass obj = new MyClass(10, "Hello World");
+		System.out.println(obj.getValue() + " " + obj.getText());
+		
 		Thread.sleep(20000);
 	}
 	
+	@AllArgsConstructor
+	@Getter
+	private static class MyClass {
+		private int value;
+		private String text;
+	}
 }
