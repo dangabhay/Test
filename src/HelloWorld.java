6d856b94ec8086a520a7badb55a727bd34354dae
Abhay Dang <dangabhay@gmail.com>
2016-06-15 10:53:17 +0530

    Adding JVM args in build.xml

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,11 +1,12 @@
 
 public final class HelloWorld {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		if (args.length > 0)
 			System.out.println("Hello " + args[0]);
 		else
 			System.out.println("No argument supplied");
+		Thread.sleep(20000);
 	}
 	
 }
